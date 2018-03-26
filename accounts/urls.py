bc135f411382f924f11df5395ffa36aaea5fb14f
pystudent1913 <zcristian1913@hotmail.com>
2018-03-26 00:08:22 -0500

    creacion de usuario

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import login, logout
 from django.urls import path
+from accounts.views import signup
 
 app_name = 'accounts'
 
 urlpatterns = [
     path("login", login, {'template_name': 'accounts/login.html'}),
-    path("logout", logout, {'next_page':'/'})    
+    path("logout", logout, {'next_page':'/'}),    
+    path("signup", signup, name='signup' )    
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def signup (request):
+    form = UserCreationForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        password = form.cleaned_data.get('password1')
+        username = form.cleaned_data.get('username')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+
+    return render(request, 'accounts/signup.html', {'form':form})
